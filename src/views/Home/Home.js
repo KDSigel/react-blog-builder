c6a19359016ce164a1504fb84677f7c831f1e11b
KDSigel <heykarl@gmail.com>
2021-11-29 15:16:34 -0800

    added empty strings to values of state

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,11 +6,11 @@ import Editor from '../../components/Editor/Editor'
 import './Home.css'
 
 export default function Home() {
-  const [title, setTitle] = useState()
-  const [subtitle, setSubtitle] = useState()
-  const [font, setFont] = useState()
-  const [align, setAlign] = useState()
-  const [text, setText] = useState()
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState('')
+  const [text, setText] = useState('')
 
   return (
     <main>
